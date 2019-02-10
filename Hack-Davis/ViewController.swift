870c6987c833f9a8081ebc887f14608b92fab827
Manohar Boppana <manoharboppana@Manohars-MacBook-Pro.local>
2019-02-10 07:21:44 -0800

    added API calls and pasrsed the JSON file

--- Hack-Davis/ViewController.swift
+++ Hack-Davis/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import AVFoundation
 import AVKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
 
@@ -23,19 +25,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //start the picture session
         setupSession()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
     
     @IBAction func captureButtonPressed(_ sender: UIButton) {
         if sender.tag == 0{
             sender.tag = 1
+            capturePhoto()
+            //time to take continuoous pictures
             captureTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(capturePhoto), userInfo: nil, repeats: true)
         }else if sender.tag == 1{
             captureTimer.invalidate()
             sender.tag = 0
         }
-//        captureTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(capturePhoto), userInfo: nil, repeats: true)
         
     }
     
@@ -108,16 +112,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             return
             
         }
-
         
+        //variable to store the UIImage
         let lastPhoto = UIImage(cgImage: cgImage, scale: 1.0, orientation: UIImage.Orientation.right)
         
-        print(lastPhoto)
-        print("UIImage generated. ")
         
+        //print(lastPhoto)
+        print("UIImage generated. ")
+        //add the image to the prieview view
         previewImageView.image = lastPhoto
+        
+
+        //API call begins
+        let heads = ["Ocp-Apim-Subscription-Key": "d6483d377e8d4711afe655aa54311b9f"] //headers for api call
+        let imgData = lastPhoto.jpegData(compressionQuality:0.8)! //compress the image
+        
+        
+        Alamofire.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imgData, withName: "fileset",fileName: "file.jpg", mimeType: "image/jpg")
+
+        },
+                         
+        to:"https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Description", method: .post, headers: heads)
+        
+        { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                
+                upload.uploadProgress(closure: { (progress) in
+                    //print("Upload Progress: \(progress.fractionCompleted)")
+                })
+                
+                upload.responseJSON { response in
+                    //print(response.result.value)
+                    let swiftyJsonVar = JSON(response.result.value!)
+                    print(swiftyJsonVar["description"]["captions"][0]["text"])
+                }
+                
+            case .failure(let encodingError):
+                print(encodingError)
+            }
+            
+        }
+        //API call ends
 
     }
-    
+
 }
 
